b146bb1d19049189a66b788f5145fc9d8b540dc4
restuak <restuaka.works@gmail.com>
2025-06-29 18:26:47 +0700

    new

--- src/app/about/components/aboutfull.tsx
+++ src/app/about/components/aboutfull.tsx
@@ -1,9 +1,9 @@
-// import GridDistortion from "./griddistortion";
+import GridDistortion from "./griddistortion";
 import ScrollVelocity from "../block/TextAnimations/ScrollVelocity/ScrollVelocity";
 
 export default function AboutFull() {
     return (
-      <main className="bg-black">
+      <main className="bg-white">
         <div className="h-screen">
           <div className="absolute top-140 w-full items-center justify-center text-center">
             <ScrollVelocity
@@ -12,16 +12,16 @@ export default function AboutFull() {
               className="bg-none text-cyan-600 text-9xl tracking-widest font-light"
             />
           </div>
-          {/* <div style={{}}>
+          <div style={{}}>
             <GridDistortion
               imageSrc="../restuaka.jpg"
-              grid={250}
-              mouse={0.1}
-              strength={0.1}
-              relaxation={0.9}
+              // grid={250}
+              // mouse={0.1}
+              // strength={0.1}
+              // relaxation={0.9}
               className="h-screen w-full"
             />
-          </div> */}
+          </div>
         </div>
       </main>
     );
