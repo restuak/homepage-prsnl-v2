07f43d5512675b3035e9289c47bec583ea816c41
restuak <restuaka.works@gmail.com>
2025-07-17 12:49:18 +0700

    feat: new add fav quotes

--- src/components/quotes.tsx
+++ src/components/quotes.tsx
@@ -8,7 +8,7 @@ export default function Quotes() {
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1.5 }}
-            className="mb-8 max-w-6xl px-8 font-serif italic font-bold text-3xl min-md:text-4xl"
+            className="mb-8 max-w-6xl px-8 font-serif italic font-bold text-zinc-400 text-3xl min-md:text-4xl"
           >
             &ldquo;I'm the dog that ate your birthday cake/ It's A Wonderful
             Life&rdquo;
@@ -19,7 +19,7 @@ export default function Quotes() {
                 initial={{ opacity: 0, y: -50 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 1.5 }}
-                className="text-lg font-light min-md:text-2xl "
+                className="text-lg text-zinc-500 font-light min-md:text-2xl "
               >
                 —Sparklehorse
               </motion.p>
